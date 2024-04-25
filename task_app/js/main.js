513faeea4a788cbdaaefed033e798096200aeeed
fu-mi-man <fumichan0393@gmail.com>
2024-04-25 20:53:21 +0900

    create task-app

---
+++ task_app/js/main.js
@@ -0,0 +1,23 @@
+const app = Vue.createApp({
+    data() {
+        return {
+            newItem: '',
+            todos: []
+        }
+    },
+    methods: {
+        addItem(event) {
+            if (this.newItem === '') return
+            let todo = {
+                item: this.newItem,
+                isDone: false,
+            }
+            this.todos.push(todo)
+            this.newItem = ''
+        },
+        deleteItem(index) {
+            this.todos.splice(index, 1)
+        }
+    }
+})
+app.mount('#app')
